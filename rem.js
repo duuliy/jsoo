838117755aa8950907d97f1dd8dda1e942d1229f
duuliy <715181149@qq.com>
2021-07-28 15:13:44 +0800

    feat: rem.js

--- rem.js
+++ rem.js
@@ -1,30 +1,41 @@
-var win = window;
-var doc = win.document;
-var docEl = doc.documentElement;
-var tid;
+(function refreshRem(window, document) {
+  var win = window
+  var doc = win.document
+  var docEl = doc.documentElement
+  var tid
 
-function refreshRem() {
-  var width = docEl.getBoundingClientRect().width;
-  var height = docEl.getBoundingClientRect().height;
-  if ((width / height) > (750 / 1334)) {
-    var rem = height / (1334 / 50);
-  } else {
-    var rem = width / (750 / 50);
+  function refreshRem() {
+    var width = docEl.getBoundingClientRect().width
+    var height = docEl.getBoundingClientRect().height
+    if ((width / height) > (750 / 1334)) {
+      var rem = height / (1334 / 50)
+    } else {
+      var rem = width / (750 / 50)
+    }
+    docEl.style.fontSize = rem + 'px'
   }
-  docEl.style.fontSize = rem + 'px';
-}
 
-win.addEventListener('resize', function () {
-  clearTimeout(tid);
-  tid = setTimeout(refreshRem, 300);
-}, false);
-win.addEventListener('pageshow', function (e) {
-  if (e.persisted) {
+  win.addEventListener('resize', function () {
     clearTimeout(tid);
-    tid = setTimeout(refreshRem, 300);
-  }
-}, false);
+    tid = setTimeout(refreshRem, 300)
+  }, false)
+  win.addEventListener('pageshow', function (e) {
+    if (e.persisted) {
+      clearTimeout(tid);
+      tid = setTimeout(refreshRem, 300);
+    }
+  }, false)
 
-refreshRem();
+  refreshRem()
 
-//屏幕宽度750/1334,比例：1:50
+  // 文字暂不启用
+  // function setBodyFontSize() {
+  //   if (document.body) {
+  //     document.body.style.fontSize = (8 * dpr) + 'px'
+  //   }
+  //   else {
+  //     document.addEventListener('DOMContentLoaded', setBodyFontSize)
+  //   }
+  // }
+  // setBodyFontSize();
+}(window, document))
